65a6c1aafa8d60df0797af5ccf3037ff9203bd1c
jazzmes <tcarvalho@cmu.edu>
2014-04-29 22:07:32 -0700

    Added functional test
    - test visualization of providers agent list

---
+++ tests/functional_tests.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import unittest
+
+class InitialProviderAgentListTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_visualization_of_provider_agents_list(self):
+        # open homepage
+        self.browser.get('http://localhost:8000')
+        self.assertIn('LAMA', self.browser.title)
+
+        self.fail('Finish test!')
+        # there is an menu option to see all provider agents
+
+        # click the option provider agents
+
+        # should see a table with a list of provider agents
+
+        # each line should contains id and ip address of the agent
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
